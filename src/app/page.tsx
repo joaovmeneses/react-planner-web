480421d56a98ce1fd9f561b814c4917d52fe0f20
Daniel <danigon1998@gmail.com>
2024-01-14 20:06:27 -0300

    Criação do menu lateral (versão 1)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
+import Content from "@/components/menu/Content";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex flex-row">
-      <Link href="/register">Register</Link>
+      {/* <Link href="/register">Register</Link> */}
+      <Content/>
     </main>
   );
 }
---
+++ src/components/menu/Content.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { useState } from "react";
+import Button from "./Button";
+
+export default function Content() {
+  const [ocultarOpcoesCiclos, setMostrarOpcoesCiclos] = useState(true);
+  const [ocultarOpcoesEdital, setMostrarOpcoesEdital] = useState(true);
+
+  return (
+    <section className="w-64 bg-purple-600 h-screen">
+      <div className="pt-14">
+        <h2 className="pl-11 mb-10 text-gray-300">PÁGINAS</h2>
+        <div>
+          <hr />
+          <Button root="/" image="./img/menu/home.svg">
+            Início
+          </Button>
+          <hr />
+          <div className={ocultarOpcoesCiclos ? "" : "flex flex-col bg-purple-800 px-4"}>
+            <div className="flex gap-24">
+              <Button image="./img/menu/circle.svg">
+                Ciclos
+              </Button>
+                <button onClick={() => setMostrarOpcoesCiclos(!ocultarOpcoesCiclos)}>
+                  {ocultarOpcoesCiclos?<img src="/img/menu/down-arrow.svg" alt="" />:<img src="/img/menu/right-arrow.svg" alt="" />}
+                </button>
+            </div>
+            <Button root="/" image="./img/menu/play-button.svg" show = {ocultarOpcoesCiclos}>
+                <p className="text-gray-300">Meus Ciclos</p>
+            </Button>
+            <Button root="/" image="./img/menu/plus.svg" show = {ocultarOpcoesCiclos}>
+            <p className="text-gray-300">Novos Ciclos</p>
+            </Button>
+          </div>
+          <hr />
+          <div className={ocultarOpcoesEdital ? "" : "flex flex-col bg-purple-800 px-4"}>
+            <div className="flex gap-24">
+              <Button image="./img/menu/file.svg">
+                Edital
+              </Button>
+                <button onClick={() => setMostrarOpcoesEdital(!ocultarOpcoesEdital)}>
+                  {ocultarOpcoesEdital?<img src="/img/menu/down-arrow.svg" alt="" />:<img src="/img/menu/right-arrow.svg" alt="" />}
+                </button>
+            </div>
+          </div>
+          <hr />
+        </div>
+      </div>
+    </section>
+  );
+}
