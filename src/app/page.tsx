0bb04476873a28ca745f6bfd48034b4554b01170
FQuental <felipe.quental@unifei.edu.br>
2024-01-31 16:34:09 -0300

    Timer com inserção de Materia e progresso

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-import Content from "@/components/menu/Content";
 import Link from "next/link";
 
 export default function Home() {
@@ -7,6 +5,8 @@ export default function Home() {
     <main className="flex flex-col justify-center bg-blue-600">
       <Link href="/register">Register</Link>
       <Link href="/site">Site</Link>
+      <Link href="/test_timer">TEST</Link>
+
     </main>
   );
 }
