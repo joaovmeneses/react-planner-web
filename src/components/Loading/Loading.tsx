260fd48e0ec189fd20acdd47125eade4dcf9557f
Gabriel Toti <gabrielsilva@byronsolutions.com>
2024-06-08 15:28:38 -0300

    Corrige posição do Loading.

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,18 +1,50 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import { useScratch } from 'react-use'
 
 interface LoadingProps {
   isLoading: boolean
 }
 
 const Loading = ({ isLoading }: LoadingProps) => {
+  const positionRef = useRef<HTMLDivElement>(null)
+  const [position, setPosition] = useState<number>()
+
+  useEffect(() => {
+    const handlePosition = () => {
+      if (!positionRef.current) return
+      const parentRect = positionRef.current.getBoundingClientRect()
+      setPosition(parentRect.left)
+    }
+
+    handlePosition()
+
+    if (positionRef.current === null) return
+
+    const resizeObserver = new ResizeObserver(handlePosition)
+
+    resizeObserver.observe(positionRef.current)
+
+    return () => {
+      if (positionRef.current === null) return
+      resizeObserver.unobserve(positionRef.current)
+    }
+  }, [isLoading])
+
   return (
     <>
       {isLoading && (
         <>
           <div className='fixed inset-0 z-40 flex justify-center items-center bg-black bg-opacity-40' />
-          <div className='fixed lg:absolute inset-0 flex flex-col justify-center items-center text-center z-50'>
-            <div className='text-white text-8xl animate-spin rounded-full tabler-loader-2' />
-            <p className='text-white text-xl font-semibold'>Loading...</p>
+          <div ref={positionRef} className='fixed lg:absolute inset-0 z-50'>
+            <div
+              className='fixed inset-x-0 inset-y-0 flex flex-col justify-center items-center text-center'
+              style={{
+                left: `${position}px`
+              }}
+            >
+              <div className='text-white text-8xl animate-spin rounded-full tabler-loader-2' />
+              <p className='text-white text-xl font-semibold'>Loading...</p>
+            </div>
           </div>
         </>
       )}
